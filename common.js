d22fcd29fc55efa615faa69655e5e771b90af651
vaibhav1811 <vs5112136@gmail.com>
2024-06-21 18:23:27 +0530

    Style Switcher

--- common.js
+++ common.js
@@ -134,3 +134,18 @@ window.addEventListener('scroll', () =>{
         styleSwitcher.classList.remove('open');
     }
 })
+
+
+const alternateStyles = document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color){
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute('title')){
+            style.removeAttribute('disabled');
+        }
+
+        else{
+            style.setAttribute('disabled', 'true');
+        }
+    })
+}
